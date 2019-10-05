5733aff0b864b0f0c51eadc7543bd8059256ad39
triaction2790 <55158751+triaction2790@users.noreply.github.com>
2019-10-05 08:01:10 -0400

    Named Entity Recognition with NLTK

---
+++ Named-Entity-8A-master/named_entity_recognition/named_entity_recognition.py
@@ -0,0 +1,42 @@
+# Named Entity Recognition with NLTK
+
+import nltk
+
+# Downloading all packages from NLTK Downloader
+nltk.download()
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+# Utilizing the state union text files in NLTK Corpus to train and test the model
+
+train_text = state_union.raw("2005-GWBush.txt")
+sample_text = state_union.raw("2006-GWBush.txt")
+
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+
+tokenized = custom_sent_tokenizer.tokenize(sample_text)
+
+def process_content():
+    try:
+        for i in tokenized[5:]:
+            words = nltk.word_tokenize(i)
+            tagged = nltk.pos_tag(words)
+            namedEnt = nltk.ne_chunk(tagged, binary=False)
+            namedEnt.draw()
+    except Exception as e:
+        print(str(e))
+
+
+process_content()
+
+# NE Type and Examples when binary is set to False
+
+''' ORGANIZATION - Georgia-Pacific Corp., WHO
+PERSON - Eddy Bonte, President Obama
+LOCATION - Murray River, Mount Everest
+DATE - June, 2008-06-29
+TIME - two fifty a m, 1:30 p.m.
+MONEY - 175 million Canadian Dollars, GBP 10.40
+PERCENT - twenty pct, 18.75 %
+FACILITY - Washington Monument, Stonehenge
+GPE - South East Asia, Midlothian '''
